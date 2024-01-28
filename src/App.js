b8a0975652a703e12eaf65a4b5683077e1c33013
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-01-28 20:11:37 +0530

    states for steps & also for open - close.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./index.css";
 
 const messages = [
@@ -7,23 +8,54 @@ const messages = [
 ];
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
+  }
+
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className="active">1</div>
-        <div>2</div>
-        <div>3</div>
-      </div>
-      <p className="message">Hello</p>
-      <div className="buttons">
-        <button style={{ backgroundColor: "#7950f9", color: "#fff" }}>
-          Back
-        </button>
-        <button style={{ backgroundColor: "#7950f9", color: "#fff" }}>
-          Next
-        </button>
-        {/* <button>Next</button> */}
-      </div>
+    <div>
+      <button
+        className="close"
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f9", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Back
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f9", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+            {/* <button>Next</button> */}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
