0c92e0198a2c2e1f0b20fd46ac41574bc4daf525
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-01-28 12:39:10 +0530

    UI development

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import "./index.css";
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="steps">
+      <div className="numbers">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+      <p className="message">Hello</p>
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f9", color: "#fff" }}>
+          Back
+        </button>
+        <button>Next</button>
+      </div>
     </div>
   );
 }
